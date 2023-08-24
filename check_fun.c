94d2f9cedb09543fce4e9de52aa2d3e9a9ac8495
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-24 06:02:53 -0700

    ok

--- check_fun.c
+++ check_fun.c
@@ -43,7 +43,7 @@ int check_fun(stack_t **top, char *arg1, char *arg2, unsigned int line_number)
 			fun_table[i].f(top, line_number);
 			if (sign == -1 && arg2[0] == '-')
 				(*top)->n = -1 * atoi(arg2), sign = -1, arg2 = &arg2[1];
-			if (strlen(arg2) > 10)
+			if (strlen(arg2) >= 10)
 			{
 				push_error(line_number);
 			}
